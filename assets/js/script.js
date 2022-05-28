1818024da9038760d9a4bc5e78b59e57ece11519
kadirula <kadirula17@gmail.com>
2022-05-28 12:10:16 +0300

    project update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,62 @@
-$(document).ready(function(){
-
-    // Toggle Search Function 
-    $('#search-icon').click(() => {
-        const headerSearch = $('.header__search');
-        headerSearch.toggleClass('show');
-    });
-
-    // Close Search Function
-    $('#close-search').click(() => {
-        const headerSearch = $('.header__search');
-        headerSearch.removeClass('show');
-    });
-
-    // Toggle Shopping Cart Function
-    $('#shopping-cart').click(() => {
-        const shoppingCart = $('.shopping-cart');
-        shoppingCart.toggleClass('show');
-    });
-
-
-    $('.count-minus').click(function(){
-        let productCount = $(this).siblings('input[type="number"]');
-
-        let productPrice = $(this).closest('.shopping-cart__input').siblings('.shopping-cart__price').children('span');
-        let price = Number(productCount.data('price'));
-
-        if(Number(productCount.val()) > 1){
-            let productCountValue = Number(productCount.val()) + -1;
-            productCount.val(productCountValue);
-            productPrice.html((Number(productCount.val()) * price).toFixed(2));
-        }
-
-        
-
-    });
-
-    $('.count-plus').click(function(){
-        let productCount = $(this).siblings('input[type="number"]');
-
-        let productPrice = $(this).closest('.shopping-cart__input').siblings('.shopping-cart__price').children('span');
-        let price = Number(productCount.data('price'));
-
-        let productCountValue = Number(productCount.val()) + 1;
-
-        productCount.val(productCountValue);
-        productPrice.html((Number(productCount.val()) * price).toFixed(2));
-
-
-        productCount.val(productCountValue);
-    });
-
+////////////////////////// SKILL PERCENT /////////////////////////////
+const skillCardItem = document.querySelectorAll('.skill-card__item');
+skillCardItem.forEach(item => {
+    const dataSkillCount = item.getAttribute("data-skill");
+    const skillCardBg = item.querySelector('.skill-card__bg');
+    const skillCardCountElement = item.querySelector('.skill-card__count');
+    skillCardBg.style.width = dataSkillCount + "%";
+    skillCardCountElement.append(dataSkillCount + "%");
 });
+
+
+////////////////////////// SECTION SHOW-REMOVE ACTION /////////////////////////////
+const sidebarMenuLink = document.querySelectorAll('.sidebar__menu-link');
+sidebarMenuLink.forEach(item => {
+
+    item.addEventListener('click', () => {
+        sidebarMenuLink.forEach(menuLink => {
+            removeElementClass(menuLink, 'active');
+        });
+        item.classList.add('active');
+        const sidebarMenuLinkData = item.getAttribute('data-id');
+        const sectionElement = document.getElementById(sidebarMenuLinkData);
+
+        const sectionElements = document.querySelectorAll('.section');
+
+        sectionElements.forEach(element => {
+            removeElementClass(element, 'active');
+        });
+
+        addElementClass(sectionElement, 'active');
+
+    })
+});
+
+
+/////////////////////////////// PORTFOLIO FILTER - ISOTOPE /////////////////////////////////////
+
+// init Isotope elements
+var $box = $(".portfolio__wrapper").isotope({
+    itemSelector: ".portfolio__item"
+});
+
+// filter functions
+// bind filter button click
+$(".portfolio__filter").on("click", "button", function () {
+    var filterValue = $(this).attr("data-type");
+    $(".portfolio__filter").find(".active").removeClass("active");
+    $(this).addClass("active");
+    if (filterValue !== "*") {
+        filterValue = '[data-type="' + filterValue + '"]';
+    }
+    $box.isotope({ filter: filterValue });
+});
+
+
+function removeElementClass(element, className) {
+    element.classList.remove(className);
+}
+
+function addElementClass(element, className) {
+    element.classList.add(className);
+}
